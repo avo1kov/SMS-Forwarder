8598782f75ca730a1637cc9ee83346406697817d
Nikita Kasatkin <bl4meme@gmail.com>
2022-10-06 19:28:40 +0300

    refactoring

--- app/src/main/kotlin/ru/smsforwarder/MainActivity.kt
+++ app/src/main/kotlin/ru/smsforwarder/MainActivity.kt
@@ -1,7 +1,7 @@
-package com.example.smsforwarder
+package ru.smsforwarder
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
